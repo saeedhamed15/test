9ca942ffa1e6559f5131d5ff3e5cd05ca65c11cf
saeedhamed15 <49121999+saeedhamed15@users.noreply.github.com>
2019-04-21 15:03:21 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`هلاتبي أي بوت أو حتا مسعد في تسميم شعر تعل خص`,"http://twitch.tv/Dream")
+client.user.setStatus("dnd")
+});
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
